d69dd9f7b67d4788cfdd63e4cc3a6ade953af5bc
Eren Kara <erenkaraaa47@gmail.com>
2022-05-17 15:34:44 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by user on 17.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
